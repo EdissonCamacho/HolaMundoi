0da3316da1596475e4adf00e25d750258afd7d6e
Edisson <eacamacho46@misena.edu.co>
2021-09-03 17:17:08 -0500

    intial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    $("#btnverifica").click(function() {
+
+        var texto = $("#texto").val();
+        var rest = texto.split('');
+
+
+
+
+        var posicion = 0;
+        var repeticion = "";
+        var contador = 0;
+
+
+        for (let index = 0; index < rest.length; index++) {
+            var letras.array = rest;
+        }
+        console.log(letras);
+
+
+    })
+
+
+
+
+
+
+
+})
